61bf3ae9705d280ed9fc9bf7ffddb5a77db95033
Anders Heyniger <anders.heyniger@gmail.com>
2025-03-01 13:28:28 -0500

    basic frontend page even though it probably wont be used

---
+++ frontend/qwerty-environmental/run_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npx next build
+npx next start
--- frontend/qwerty-environmental/src/app/page copy.js
+++ frontend/qwerty-environmental/src/app/page copy.js
@@ -1,15 +1,13 @@
-import Image from "next/image";
-import React from 'react';
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen bg-fixed bg-cover bg-center" style={{ backgroundImage: "url('/background.jpeg')" }}>
+    <div className="relative h-screen w-screen bg-fixed bg-cover bg-center" style={{ backgroundImage: "url('/background.jpg')" }}>
       <Head>
         <title>Static Background Page</title>
       </Head>
       <div className="absolute inset-0 flex justify-center items-center">
-        <div className="bg-black bg-opacity-80 rounded-lg shadow-lg p-6 max-w-5xl w-full h-3/4 overflow-y-auto">
+        <div className="bg-white bg-opacity-80 rounded-lg shadow-lg p-6 max-w-2xl w-full h-3/4 overflow-y-auto">
           <h1 className="text-2xl font-bold mb-4">Scrollable Content</h1>
           <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
           <p className="mb-2">More content here...</p>
